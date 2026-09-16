94a7e7e5c57b994694a578174b5cf8cff21b939c
write-code-b <minjichoi.official@gmail.com>
2026-09-16 23:13:12 +0900

    Update Marquee

--- components/Marquee.tsx
+++ components/Marquee.tsx
@@ -5,7 +5,7 @@ import { motion } from 'motion/react'
 const Marquee = () => {
   return (
     <>
-      <div className="overflow-hidden py-3">
+      <div className="mt-1 overflow-hidden bg-black font-bold">
         <motion.div
           className="flex w-max text-sm"
           animate={{ x: ['0%', '-50%'] }}
@@ -15,14 +15,14 @@ const Marquee = () => {
             repeat: Infinity,
           }}
         >
-          <span className="mr-4 text-(--color-accent)">
-            시장투어 웹진을 만들었어용 💚 시장투어 웹진을 만들었어용 💚 시장투어 웹진을 만들었어용
-            💚 시장투어 웹진을 만들었어용 💚 시장투어 웹진을 만들었어용 💚
+          <span className="mr-4 text-(--color-background)">
+            먹고사는 이야기로 가득한 웹진을 만들어 보려고 합니다. 먹고사는 이야기로 가득한 웹진을
+            만들어 보려고 합니다. 먹고사는 이야기로 가득한 웹진을 만들어 보려고 합니다.
           </span>
 
-          <span className="mr-4 text-(--color-accent)">
-            시장투어 웹진을 만들었어용 💚 시장투어 웹진을 만들었어용 💚 시장투어 웹진을 만들었어용
-            💚 시장투어 웹진을 만들었어용 💚 시장투어 웹진을 만들었어용 💚
+          <span className="mr-4 text-(--color-background)">
+            먹고사는 이야기로 가득한 웹진을 만들어 보려고 합니다. 먹고사는 이야기로 가득한 웹진을
+            만들어 보려고 합니다. 먹고사는 이야기로 가득한 웹진을 만들어 보려고 합니다.
           </span>
         </motion.div>
       </div>
